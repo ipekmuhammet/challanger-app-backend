f0dd3b1c80fdac5fa48d21d9ca932b30e601dfb3
Muhammet <muhammetipek57@hotmail.com>
2019-10-04 01:34:14 +0300

    01.34

--- graphql/resolvers/mutations/block.mutation.js
+++ graphql/resolvers/mutations/block.mutation.js
@@ -1,5 +1,6 @@
 const Mutation = {
-    saveBlock: async (source, { data }, { Block }) => await new Block(data).save()
+    saveBlock: async (source, { data }, { Block }) => await new Block(data).save(),
+    deleteBlock: async (source, { data }, { Block }) => await Block.findByIdAndDelete(data.id)
 }
 
 module.exports = Mutation;
