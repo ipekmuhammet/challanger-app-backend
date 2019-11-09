2c443beaee0ef01157822391105f4dcb7d913026
Muhammet <muhammetipek57@hotmail.com>
2019-11-10 01:13:18 +0300

    messages finding problem solved

--- graphql/resolvers/queries/Message.js
+++ graphql/resolvers/queries/Message.js
@@ -1,6 +1,10 @@
+let user_id
+
 module.exports = {
     user: async (source, args, { User }) => source,
-    messages: async (source, args, { Message }) => await Message.find({ sender: source._id })
-    //Muhammet - this is a sub resolver
-    //find messages {sender:rootResolver.user_id,receiver:source._id} || {sender:source._id,receiver:rootResolver.user_id}
+    messages: async (source, args, { Message }, info) => {
+        user_id = info.operation.selectionSet.selections[0].arguments[0].value.fields[0].value.value
+        return await Message.find()
+            .or([{ sender: user_id, receiver: source._id }, { sender: source._id, receiver: user_id }])
+    }
 }
--- graphql/resolvers/queries/Query.js
+++ graphql/resolvers/queries/Query.js
@@ -15,7 +15,7 @@ module.exports = {
         if (target_id) return await Post.find({ user_id: target_id })
         else if (user_id) {
             follows = await Follow.find({ follower: user_id }).map(follow => follow = follow[0].followed)//Muhammet
-            return await Post.where('user_id').in(follows).limit(20).find({})
+            return await Post.where('user_id').in(follows).limit(20).find()
         }
         else if (key) return await Post.find({ content: { $regex: key, $options: 'i' } }).limit(20)
         else if (latest) return await Post
@@ -24,7 +24,7 @@ module.exports = {
             .sort({ createdAt: 'descending' })
         return []
     },
-    listChallanges: async (source, { data }, { Challange }) => await Challange.find({}).limit(20),
+    listChallanges: async (source, { data }, { Challange }) => await Challange.find().limit(20),
 
     listComments: async (source, { data: { post_id } }, { Comment }) => await Comment.find({ post_id }).limit(20),
 
@@ -34,7 +34,7 @@ module.exports = {
 
     listFollows: async (source, { data: { follower } }, { Follow }) => await Follow.find({ follower }).limit(20),
 
-    listMessages: async (source, { data: { user_id, chats } }, { User }, info) => await User.where('_id').in(chats).find({}),
+    listMessages: async (source, { data: { user_id, chats } }, { User }, info) => await User.where('_id').in(chats).find(),
 
     listBlocks: async (source, { data: { blocker } }, { Block }) => await Block.find({ blocker })
 }
