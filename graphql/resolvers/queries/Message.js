3ca9c5dec409a8a2772b2bf36a2a7fb693e2e916
Muhammet <muhammetipek57@hotmail.com>
2019-11-10 19:14:50 +0300

    19:15

--- graphql/resolvers/queries/Message.js
+++ graphql/resolvers/queries/Message.js
@@ -1,10 +1,6 @@
-let user_id
-
 module.exports = {
     user: async (source, args, { User }) => source,
-    messages: async (source, args, { Message }, info) => {
-        user_id = info.operation.selectionSet.selections[0].arguments[0].value.fields[0].value.value
-        return await Message.find()
-            .or([{ sender: user_id, receiver: source._id }, { sender: source._id, receiver: user_id }])
-    }
+    messages: async (source, args, { Message }, { variableValues: { user_id } }) => (
+        await Message.find().or([{ sender: user_id, receiver: source._id }, { sender: source._id, receiver: user_id }])
+    )
 }
